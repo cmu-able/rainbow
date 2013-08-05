340e24a9382163bd93d0efd3ad16b2a7a8225dbd
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2013-08-05 18:53:56 +0000

    Improvements to event generation and testing.
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1192 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-acme-model/src/test/java/modeltests/TestModelTransactions.java
+++ rainbow-acme-model/src/test/java/modeltests/TestModelTransactions.java
@@ -1,6 +1,9 @@
 package modeltests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,9 +20,11 @@ import org.sa.rainbow.core.error.RainbowConnectionException;
 import org.sa.rainbow.core.error.RainbowException;
 import org.sa.rainbow.core.event.IRainbowMessage;
 import org.sa.rainbow.management.ports.eseb.ESEBConstants;
+import org.sa.rainbow.model.acme.AcmeRainbowCommandEvent.CommandEventT;
 import org.sa.rainbow.model.acme.znn.commands.NewServerCmd;
 import org.sa.rainbow.models.IModelInstance;
 import org.sa.rainbow.models.commands.IRainbowModelCommandRepresentation;
+import org.sa.rainbow.models.ports.IRainbowModelChangeBusPort;
 
 import edu.cmu.cs.able.eseb.BusData;
 import edu.cmu.cs.able.eseb.BusDataQueue;
@@ -33,7 +38,6 @@ public class TestModelTransactions {
 
     private RainbowMaster master;
 
-    
     @Test
     public void testEventsPropagatedOk () throws Exception {
         PrimitiveScope scope = new PrimitiveScope ();
@@ -58,34 +62,41 @@ public class TestModelTransactions {
             NewServerCmd cmd = new NewServerCmd ("newServer", modelInstance.getModelInstance (), "lbproxy", "server");
             master.modelsManager ().requestModelUpdate (cmd);
             List<? extends IRainbowMessage> generatedEvents = cmd.getGeneratedEvents ();
+            // each event should have a MSG_TYPE_KEY
             Thread.sleep (500);
             for (DataValue dv : vals) {
                 assertTrue (dv instanceof MapDataValue);
                 MapDataValue mdv = (MapDataValue )dv;
                 assertTrue (mdv.all ().containsKey (scope.string ().make (ESEBConstants.MSG_TYPE_KEY)));
             }
+            // size of events on the client should be the same as the size of the events generated
             assertEquals (generatedEvents.size (), vals.size ());
+
+            // The first and last events should be START and FINISH
+            assertEquals (generatedEvents.get (0).getProperty (IRainbowModelChangeBusPort.EVENT_TYPE_PROP),
+                    CommandEventT.START_COMMAND.name ());
+            assertEquals (
+                    generatedEvents.get (generatedEvents.size () - 1).getProperty (
+                            IRainbowModelChangeBusPort.EVENT_TYPE_PROP), CommandEventT.FINISH_COMMAND.name ());
         }
     }
-    
+
     @Test
     public void testTransactionOK () throws Exception {
         assertTrue (master.modelsManager () != null);
         assertTrue (!master.modelsManager ().getRegisteredModelTypes ().isEmpty ());
         assertTrue (master.modelsManager ().getRegisteredModelTypes ().contains ("Acme"));
 
-        
+        IModelInstance<IAcmeSystem> modelInstance = master.modelsManager ().<IAcmeSystem> getModelInstance ("Acme",
+                "ZNewsSys");
 
-            IModelInstance<IAcmeSystem> modelInstance = master.modelsManager ().<IAcmeSystem> getModelInstance ("Acme",
-                    "ZNewsSys");
+        List<IRainbowModelCommandRepresentation> commands = new LinkedList<> ();
+        commands.add (new NewServerCmd ("newServer", modelInstance.getModelInstance (), "lbproxy", "server"));
+        commands.add (new NewServerCmd ("newServer", modelInstance.getModelInstance (), "lbproxy", "server"));
+        master.modelsManager ().requestModelUpdate (commands, true);
 
-            List<IRainbowModelCommandRepresentation> commands = new LinkedList<> ();
-            commands.add (new NewServerCmd ("newServer", modelInstance.getModelInstance (), "lbproxy", "server"));
-            commands.add (new NewServerCmd ("newServer", modelInstance.getModelInstance (), "lbproxy", "server"));
-            master.modelsManager ().requestModelUpdate (commands, true);
-
-            assertNotNull (modelInstance.getModelInstance ().getComponent ("server"));
-            assertNotNull (modelInstance.getModelInstance ().getComponent ("server0"));
+        assertNotNull (modelInstance.getModelInstance ().getComponent ("server"));
+        assertNotNull (modelInstance.getModelInstance ().getComponent ("server0"));
     }
 
     @Test
@@ -139,7 +150,9 @@ public class TestModelTransactions {
 
     @After
     public void shutdown () {
-        if (master != null) master.terminate ();
+        if (master != null) {
+            master.terminate ();
+        }
         master = null;
     }
 
