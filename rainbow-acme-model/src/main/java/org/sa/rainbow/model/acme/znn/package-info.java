91338a701ff45a043b5dbac4aaf745e66adc5373
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2013-10-18 22:08:29 +0000

    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1224 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-acme-model/src/main/java/org/sa/rainbow/model/acme/znn/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * This package is purely for testing purposes. It should be move to a new project when all is said and done
- * @author Bradley Schmerl: schmerl
- *
- */
-package org.sa.rainbow.model.acme.znn;
