8665a44a5264696ccafcd7ac4e0d84522933fe57
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2014-10-03 15:22:13 +0000

    License prepended to source files
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1321 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-znn/src/main/java/org/sa/rainbow/model/acme/znn/commands/SetNumRedirectedRequestsCmd.java
+++ rainbow-znn/src/main/java/org/sa/rainbow/model/acme/znn/commands/SetNumRedirectedRequestsCmd.java
@@ -1,3 +1,29 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 CMU Aimport java.util.LinkedList;
+import java.util.List;
+
+import org.acmestudio.acme.PropertyHelper;
+import org.acmestudio.acme.element.IAcmeConnector;
+import org.acmestudio.acme.element.property.IAcmeProperty;
+import org.acmestudio.acme.model.command.IAcmeCommand;
+import org.acmestudio.acme.model.command.IAcmePropertyCommand;
+import org.sa.rainbow.core.error.RainbowModelException;
+import org.sa.rainbow.model.acme.AcmeModelInstance;
+ to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.sa.rainbow.model.acme.znn.commands;
 
 import java.util.LinkedList;
