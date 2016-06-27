e5a4e1a330433c39661713c86d5e34b92287adf5
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2016-06-27 13:25:12 +0000

    Made sure parallel test is syncrhonized.
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1372 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-core/src/test/java/org/sa/rainbow/core/adaptation/AdaptationTreeTest.java
+++ rainbow-core/src/test/java/org/sa/rainbow/core/adaptation/AdaptationTreeTest.java
@@ -1,21 +1,116 @@
 package org.sa.rainbow.core.adaptation;
 
+import auxtestlib.DefaultTCase;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sa.rainbow.core.RainbowComponentT;
+import org.sa.rainbow.core.ports.IRainbowReportingPort;
+
+import java.text.MessageFormat;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import auxtestlib.DefaultTCase;
-
 public class AdaptationTreeTest extends DefaultTCase {
 
+    private static final class TestCaseReportingPort implements IRainbowReportingPort {
+        Logger L = Logger.getLogger (TestCaseReportingPort.class);
+
+        @Override
+        public void dispose () {
+        }
+
+        @Override
+        public void warn (RainbowComponentT type, String msg) {
+            L.warn (composeMessage (type, msg));
+        }
+
+        String composeMessage (RainbowComponentT type, String msg) {
+            return MessageFormat.format ("{0}: {1}", type.name (), msg);
+        }
+
+        @Override
+        public void warn (RainbowComponentT type, String msg, Throwable e) {
+            L.warn (composeMessage (type, msg), e);
+        }
+
+        @Override
+        public void warn (RainbowComponentT type, String msg, Logger logger) {
+            logger.warn (composeMessage (type, msg));
+        }
+
+        @Override
+        public void warn (RainbowComponentT type, String msg, Throwable e, Logger logger) {
+            logger.warn (composeMessage (type, msg), e);
+
+        }
+
+        @Override
+        public void trace (RainbowComponentT type, String msg) {
+            L.trace (composeMessage (type, msg));
+        }
+
+        @Override
+        public void info (RainbowComponentT type, String msg) {
+            L.info (composeMessage (type, msg));
+
+        }
+
+        @Override
+        public void info (RainbowComponentT type, String msg, Logger logger) {
+            logger.info (composeMessage (type, msg));
+
+        }
+
+        @Override
+        public void fatal (RainbowComponentT type, String msg) {
+            L.fatal (composeMessage (type, msg));
+        }
+
+        @Override
+        public void fatal (RainbowComponentT type, String msg, Throwable e) {
+            L.fatal (composeMessage (type, msg), e);
+        }
+
+        @Override
+        public void fatal (RainbowComponentT type, String msg, Logger logger) {
+            logger.fatal (composeMessage (type, msg));
+        }
+
+        @Override
+        public void fatal (RainbowComponentT type, String msg, Throwable e, Logger logger) {
+            logger.fatal (composeMessage (type, msg), e);
+        }
+
+        @Override
+        public void error (RainbowComponentT type, String msg) {
+            L.error (composeMessage (type, msg));
+
+        }
+
+        @Override
+        public void error (RainbowComponentT type, String msg, Throwable e) {
+            L.error (composeMessage (type, msg), e);
+        }
+
+        @Override
+        public void error (RainbowComponentT type, String msg, Logger logger) {
+            logger.error (composeMessage (type, msg));
+        }
+
+        @Override
+        public void error (RainbowComponentT type, String msg, Throwable e, Logger logger) {
+            logger.error (composeMessage (type, msg), e);
+        }
+    }
+
     private final class TestAdaptationVisitor extends DefaultAdaptationExecutorVisitor<IEvaluable> {
+
         private TestAdaptationVisitor (AdaptationTree<IEvaluable> adt, ThreadGroup tg, CountDownLatch done) {
-            super (adt, tg, "", done);
+            super (adt, tg, "", done, new TestCaseReportingPort ());
         }
 
         @Override
@@ -48,7 +143,9 @@ public class AdaptationTreeTest extends DefaultTCase {
 
         @Override
         public Object evaluate (Object[] argsIn) {
-            numbers.add (m_number);
+            synchronized (numbers) {
+                numbers.add (m_number);
+            }
             return m_return;
         }
 
@@ -59,7 +156,7 @@ public class AdaptationTreeTest extends DefaultTCase {
 
     }
 
-    List<Integer> numbers = new LinkedList ();
+    private List<Integer> numbers = new LinkedList ();
 
     @Before
     public void setUp () throws Exception {
@@ -72,7 +169,7 @@ public class AdaptationTreeTest extends DefaultTCase {
 
     @Test
     public void testSequence () throws InterruptedException {
-        AdaptationTree root = new AdaptationTree<IEvaluable> (AdaptationExecutionOperatorT.SEQUENCE);
+        AdaptationTree root = new AdaptationTree<> (AdaptationExecutionOperatorT.SEQUENCE);
         root.addLeaf (new ExecutableTest (1));
         root.addLeaf (new ExecutableTest (2));
         root.addLeaf (new ExecutableTest (3));
@@ -80,7 +177,7 @@ public class AdaptationTreeTest extends DefaultTCase {
         CountDownLatch countdownLatch = new CountDownLatch (1);
 
         DefaultAdaptationExecutorVisitor<IEvaluable> visitor = new DefaultAdaptationExecutorVisitor<IEvaluable> (root,
-                new ThreadGroup ("execution"), "", countdownLatch) {
+                                                                                                                 new ThreadGroup ("execution"), "", countdownLatch, new TestCaseReportingPort ()) {
 
             @Override
             protected boolean evaluate (IEvaluable adaptation) {
@@ -105,15 +202,15 @@ public class AdaptationTreeTest extends DefaultTCase {
 
     @Test
     public void testSequenceFail () throws InterruptedException {
-        AdaptationTree root = new AdaptationTree<IEvaluable> (AdaptationExecutionOperatorT.SEQUENCE_STOP_FAILURE);
+        AdaptationTree root = new AdaptationTree<> (AdaptationExecutionOperatorT.SEQUENCE_STOP_FAILURE);
         root.addLeaf (new ExecutableTest (1));
         root.addLeaf (new ExecutableTest (2, false));
         root.addLeaf (new ExecutableTest (3));
 
         CountDownLatch countdownLatch = new CountDownLatch (1);
 
-        DefaultAdaptationExecutorVisitor<IEvaluable> visitor = new TestAdaptationVisitor (root, new ThreadGroup (
-                "execution"), countdownLatch);
+        DefaultAdaptationExecutorVisitor<IEvaluable> visitor = new TestAdaptationVisitor (root,
+                                                                                          new ThreadGroup ("execution"), countdownLatch);
         visitor.start ();
         countdownLatch.await (5, TimeUnit.SECONDS);
         assertTrue (numbers.size () == 2);
@@ -123,7 +220,7 @@ public class AdaptationTreeTest extends DefaultTCase {
 
     @Test
     public void testSequenceSuccess () throws InterruptedException {
-        AdaptationTree root = new AdaptationTree<IEvaluable> (AdaptationExecutionOperatorT.SEQUENCE_STOP_SUCCESS);
+        AdaptationTree root = new AdaptationTree<> (AdaptationExecutionOperatorT.SEQUENCE_STOP_SUCCESS);
         root.addLeaf (new ExecutableTest (1));
         root.addLeaf (new ExecutableTest (2, false));
         root.addLeaf (new ExecutableTest (3));
@@ -141,7 +238,7 @@ public class AdaptationTreeTest extends DefaultTCase {
 
     @Test
     public void testParallel () throws InterruptedException {
-        AdaptationTree root = new AdaptationTree<IEvaluable> (AdaptationExecutionOperatorT.PARALLEL);
+        AdaptationTree root = new AdaptationTree<> (AdaptationExecutionOperatorT.PARALLEL);
         int number = 20;
         for (int i = 0; i < number; i++) {
             root.addLeaf (new ExecutableTest (i));
@@ -149,8 +246,8 @@ public class AdaptationTreeTest extends DefaultTCase {
 
         CountDownLatch countdownLatch = new CountDownLatch (1);
 
-        DefaultAdaptationExecutorVisitor<IEvaluable> visitor = new TestAdaptationVisitor (root, new ThreadGroup (
-                "execution"), countdownLatch);
+        DefaultAdaptationExecutorVisitor<IEvaluable> visitor = new TestAdaptationVisitor (root,
+                                                                                          new ThreadGroup ("execution"), countdownLatch);
         visitor.start ();
         countdownLatch.await (5, TimeUnit.SECONDS);
         assertTrue (numbers.size () == number);
