f53e4636e0324e5ef855ee1fb93729d2c4a8c8de
jcmoreno <jcmoreno@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2017-01-24 20:00:22 +0000

    Fixed NewInstructionGraph instantiation in BRASSPlanner
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1419 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-brass/src/main/java/org/sa/rainbow/brass/adaptation/BRASSAdaptationPlanner.java
+++ rainbow-brass/src/main/java/org/sa/rainbow/brass/adaptation/BRASSAdaptationPlanner.java
@@ -166,8 +166,7 @@ implements IAdaptationManager<BrassPlan>, IRainbowModelChangeCallback {
                  PrismPolicy prismPolicy = new PrismPolicy(pc.getPrismPolicyLocation());
        		  	 prismPolicy.readPolicy();  
        		  	 PolicyToIG translator = new PolicyToIG(prismPolicy, map);
-       		     //NewInstructionGraph nig = NewInstructionGraph(igModel, translator.translate()); // Ashutosh: do this
-       		  	 NewInstructionGraph nig = null;
+       		     NewInstructionGraph nig = new NewInstructionGraph(igModel, translator.translate()); // Ashutosh: do this
                  AdaptationTree<BrassPlan> at = new AdaptationTree<BrassPlan> (nig);
                  m_adaptationEnqueuePort.offerAdaptation (at, new Object[] {});
              }
