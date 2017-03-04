dd30d3cb12c3d5ccc08cc55d3fe76e426cff940c
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2017-03-04 20:59:02 +0000

    Definitions for CP2
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1499 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-brass/brass-cp2/system/effectors/setCalibration.sh
+++ rainbow-brass/brass-cp2/system/effectors/setCalibration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 . /opt/ros/indigo/setup.bash
 . ~/catkin_ws/devel/setup.bash
-rostopic pub /calibration/recalibrate std_msgs/String \"recalibrate\"
+echo "Attempting to recalibrate"
+rostopic pub -1 /calibration/commands std_msgs/String \"recalibrate\"
