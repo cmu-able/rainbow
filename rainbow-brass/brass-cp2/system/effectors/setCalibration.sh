4d330c3d8971cef0a7e839fb1ec323f0988c878b
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2017-03-10 13:46:07 +0000

    Changes for the code freeze
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1507 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-brass/brass-cp2/system/effectors/setCalibration.sh
+++ rainbow-brass/brass-cp2/system/effectors/setCalibration.sh
@@ -2,4 +2,5 @@
 . /opt/ros/indigo/setup.bash
 . ~/catkin_ws/devel/setup.bash
 echo "Attempting to recalibrate"
+python ~/catkin_ws/src/ig_action_server/ig_action_client/src/ig_client.py CANCEL
 rostopic pub -1 /calibration/commands std_msgs/String \"recalibrate\"
--- rainbow-brass/brass-cp2/system/effectors/setInstructions.sh
+++ rainbow-brass/brass-cp2/system/effectors/setInstructions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$@" >> /tmp/ig
+echo "$@" > /tmp/ig
 . /opt/ros/indigo/setup.bash
 . ~/catkin_ws/devel/setup.bash
 python ~/catkin_ws/src/ig_action_server/ig_action_client/src/ig_client.py /tmp/ig
