1fd177412f6643c2bc543362a91924f518525433
bradley <bradley@eea7622e-ad16-0410-96ee-d91cb3d3ee64>
2017-03-18 20:26:12 +0000

    Better policy for reporting when adaptations are initiated and completed. Reorganization of the adaptation planner.
    git-svn-id: svn://acme.able.cs.cmu.edu/rainbow/v2/trunk@1517 eea7622e-ad16-0410-96ee-d91cb3d3ee64

--- rainbow-brass/src/main/java/org/sa/rainbow/brass/das/IBRASSConnector.java
+++ rainbow-brass/src/main/java/org/sa/rainbow/brass/das/IBRASSConnector.java
@@ -10,6 +10,7 @@ public interface IBRASSConnector {
         MISSION_ABORTED, // indicates that an adaptive response is deemed to be infeasible
         ADAPTING, //  indicates that the selection/generation of an adaptive version of the target software system has begun or is in progress
         ADAPTATION_COMPLETED, //  indicates that the adaptation response is done, and the adaptive version of the target software system has been deployed
+        ADAPTATION_INITIATED,
         TEST_ERROR,
         // Rainbow specific messages for brasscomms
         RAINBOW_READY, MISSION_COMPLETED
