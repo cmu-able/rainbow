7f4bf45f52725724fc83da471b8c1ae0adeb93de
Bradley Schmerl <schmerl@cs.cmu.edu>
2017-11-02 18:16:10 -0400

    Bug fix on sendAndReceive to construct a replyQ to receive replies.

--- rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/ports/eseb/ESEBConnector.java
+++ rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/ports/eseb/ESEBConnector.java
@@ -42,17 +42,16 @@ import java.util.UUID;
 
 /**
  * The ESEBConnector implements both a publish/subscribe and call-return connection over ESEB.
- * 
+ * <p/>
  * If the connector is created with a remoteHost and a remotePort, a client is started, connecting to the server on that
  * port. (N.B. it is assumed that a server is started)
- * 
+ * <p/>
  * If the connector is created with just a port, then a server is started on that port, and a client is created to
  * publish and receive information. If a connector is created with the same port as used previously, then the caches
  * server will be used. If a client is requested on a previously used remote host and port, then the cached client will
  * be used.
- * 
+ *
  * @author Bradley Schmerl: schmerl
- * 
  */
 public class ESEBConnector {
     static final Logger LOGGER = Logger.getLogger (ESEBConnector.class);
@@ -64,37 +63,39 @@ public class ESEBConnector {
 
     /**
      * An interface that allows users to receive information that is published on the bus
-     * 
+     *
      * @author Bradley Schmerl: schmerl
-     * 
      */
     public interface IESEBListener {
         /**
          * Receive a message from ESEB
-         * 
-         * @param msg
-         *            The message that is received
+         *
+         * @param msg The message that is received
          */
         void receive (RainbowESEBMessage msg);
     }
 
 
-    /** The server corresponding to this connector **/
+    /**
+     * The server corresponding to this connector
+     **/
     private EventBus      m_srvr;
-    /** The client for this connector to publish and receive information **/
+    /**
+     * The client for this connector to publish and receive information
+     **/
 
     private BusConnection m_client;
-    /** The set of listeners that are awaiting replies. **/
+    /**
+     * The set of listeners that are awaiting replies.
+     **/
     private static final Map<String, IESEBListener> m_replyListeners = new HashMap<> ();
     private final ChannelT m_channel;
 
     /**
      * Constructs a new ESEBConnector, connecting to a remote bus
      *
-     * @param remoteHost
-     *            The host to connect to
-     * @param remotePort
-     *            The port to connect to
+     * @param remoteHost The host to connect to
+     * @param remotePort The port to connect to
      * @throws IOException
      */
     public ESEBConnector (String remoteHost, short remotePort, ChannelT channel) {
@@ -107,8 +108,7 @@ public class ESEBConnector {
     /**
      * Construct a new ESEBConnector locally on the specified port.
      *
-     * @param port
-     *            The port to connect to
+     * @param port The port to connect to
      * @throws IOException
      */
     public ESEBConnector (short port, ChannelT channel) throws IOException {
@@ -145,25 +145,25 @@ public class ESEBConnector {
     /**
      * Publishes a map onto the bus
      *
-     * @param msg
-     *            the message to publish, as key/value pairs
+     * @param msg the message to publish, as key/value pairs
      */
     public void publish (RainbowESEBMessage msg) {
         msg.setProperty (ESEBConstants.MSG_SENT, System.currentTimeMillis ());
         m_client.send (msg.getDataValue ());
     }
 
+    private BusDataQueue replyQ = null;
+
     /**
      * Implements a call and return connection over the bus. The call is represented in msg, which is sent to the bus.
      * The receiveListener is what is called when a call is responded to. This is done asynchronously, so clients who
      * want to block will need to handle their own blocking.
      *
-     * @param msg
-     *            The call
-     * @param receiveListener
-     *            The listener to call when a response returns
+     * @param msg             The call
+     * @param receiveListener The listener to call when a response returns
      */
     public void sendAndReceive (RainbowESEBMessage msg, final IESEBListener receiveListener) {
+
         // Generate a random reply key and put it in the message so that responder knows how to respond
         final String replyKey = UUID.randomUUID ().toString ();
         msg.setProperty (ESEBConstants.MSG_REPLY_KEY, replyKey);
@@ -176,52 +176,65 @@ public class ESEBConnector {
         // Add a reply queue that will process the reply 
         // Note that if we want the send/receive to be blocking, then that needs to be processed
         // by the client.
-        final BusDataQueue replyQ = new BusDataQueue ();
-        m_client.queue_group ().add (replyQ);
+        if (replyQ == null) {
+        /*final BusDataQueue*/
+            replyQ = new BusDataQueue ();
+            m_client.queue_group ().add (replyQ);
 
-        replyQ.dispatcher ().add (new BusDataQueueListener () {
-            @Override
-            public void data_added_to_queue () {
-                BusData bd;
-                while ((bd = replyQ.poll ()) != null) {
-                    DataValue v = bd.value ();
-                    if (v instanceof MapDataValue) {
-                        MapDataValue mdv = (MapDataValue )v;
-                        RainbowESEBMessage msg = new RainbowESEBMessage (mdv);
-                        if (!msg.getProperty (ESEBConstants.MSG_CHANNEL_KEY).equals (m_channel.name ())) {
-                            continue;
+            replyQ.dispatcher ().add (new BusDataQueueListener () {
+                @Override
+                public void data_added_to_queue () {
+                    BusData bd;
+                    while ((bd = replyQ.poll ()) != null) {
+                        DataValue v = bd.value ();
+                        if (v instanceof MapDataValue) {
+                            MapDataValue mdv = (MapDataValue) v;
+                            RainbowESEBMessage msg = new RainbowESEBMessage (mdv);
+                            if (!msg.getProperty (ESEBConstants.MSG_CHANNEL_KEY).equals (m_channel.name ())) {
+                                continue;
+                            }
+                            String repKey = (String) msg.getProperty (ESEBConstants.MSG_REPLY_KEY);
+                            Object msgType = msg.getProperty (ESEBConstants.MSG_TYPE_KEY);
+
+                            // Make sure that the message is a reply, otherwise ignore
+                            // TODO: Bug --> if it isn't a reply, we've removed it so others now cannot process it
+                            if (ESEBConstants.MSG_TYPE_REPLY.equals (msgType)) {
+                                // Get the associated reply listener
+                                IESEBListener l;
+                                synchronized (m_replyListeners) {
+                                    l = m_replyListeners.remove (repKey);
+                                }
+                                // Call the listener
+                                if (l != null) {
+//                                    LOGGER.info ("Processing reply: " + msg.toString ());
+                                    sanitizeMessage (msg);
+                                    l.receive (msg);
+                                } else {
+                                    LOGGER.error (MessageFormat.format (
+                                            "Received a reply on ESEB for which there is no listener. For reply key: {0}",
+
+                                            repKey));
+                                    LOGGER.info (msg.toString ());
+                                    synchronized (m_replyListeners) {
+                                        for (String rk : m_replyListeners.keySet ()) {
+                                            LOGGER.info (rk + " -> " + m_replyListeners.get (rk));
+                                        }
+                                    }
+                                }
+                                // replyQ.dispatcher ().remove (this);
+                                // m_client.queue_group ().remove (replyQ);
+
+                            }
+
                         }
-                        String repKey = (String )msg.getProperty (ESEBConstants.MSG_REPLY_KEY);
-                        Object msgType = msg.getProperty (ESEBConstants.MSG_TYPE_KEY);
-
-                        // Make sure that the message is a reply, otherwise ignore
-                        // TODO: Bug --> if it isn't a reply, we've removed it so others now cannot process it
-                        if (ESEBConstants.MSG_TYPE_REPLY.equals (msgType)) {
-                            // Get the associated reply listener
-                            IESEBListener l;
-                            synchronized (m_replyListeners) {
-                                l = m_replyListeners.remove (repKey);
-                            }
-                            // Call the listener
-                            if (l != null) {
-                                sanitizeMessage (msg);
-                                l.receive (msg);
-                            }
-                            else {
-                                LOGGER.error (MessageFormat.format (
-                                        "Received a reply on ESEB for which there is no listener. For reply key: {0}",
-                                        repKey));
-                            }
-                            replyQ.dispatcher ().remove (this);
-                            m_client.queue_group ().remove (replyQ);
-                        }
-
                     }
                 }
-            }
-        });
+            });
+        }
 
         // send the call
+//        LOGGER.info ("Publishing " + msg.toString ());
+
         publish (msg);
 
     }
@@ -229,7 +242,7 @@ public class ESEBConnector {
     public class BlockingListener implements IESEBListener {
 
         private final IESEBListener m_l;
-        boolean               ret = false;
+        boolean ret = false;
 
         public BlockingListener (IESEBListener l) {
             m_l = l;
@@ -254,9 +267,8 @@ public class ESEBConnector {
             sendAndReceive (msg, bl);
             try {
                 // TODO: THis should be uncommented
-                bl.wait (/*timeout*/);
-            }
-            catch (InterruptedException e) {
+                bl.wait (timeout);
+            } catch (InterruptedException e) {
             }
         }
         if (!bl.ret)
@@ -266,9 +278,8 @@ public class ESEBConnector {
 
     /**
      * Removes any internal key, value pairs from the message
-     * 
-     * @param msg
-     *            The message to sanitize
+     *
+     * @param msg The message to sanitize
      */
     private void sanitizeMessage (RainbowESEBMessage msg) {
         msg.removeProperty (ESEBConstants.MSG_REPLY_KEY);
@@ -277,9 +288,8 @@ public class ESEBConnector {
 
     /**
      * Adds a listener to the client queue.
-     * 
-     * @param l
-     *            The listener to call if a message is received.
+     *
+     * @param l The listener to call if a message is received.
      */
     public void addListener (final IESEBListener l) {
         // Set up the queues
@@ -293,14 +303,13 @@ public class ESEBConnector {
                 while ((bd = clientReceiveQ.poll ()) != null) {
                     DataValue v = bd.value ();
                     if (v instanceof MapDataValue) {
-                        MapDataValue mdv = (MapDataValue )v;
+                        MapDataValue mdv = (MapDataValue) v;
                         RainbowESEBMessage msg = new RainbowESEBMessage (mdv);
                         if (!msg.getProperty (ESEBConstants.MSG_CHANNEL_KEY).equals (m_channel.name ())) {
                             continue;
                         }
                         // Ignore any replies on this queue
-                        if (!ESEBConstants.MSG_TYPE_REPLY.equals (msg.getProperty (ESEBConstants.MSG_TYPE_KEY)))
-                        {
+                        if (!ESEBConstants.MSG_TYPE_REPLY.equals (msg.getProperty (ESEBConstants.MSG_TYPE_KEY))) {
                             l.receive (msg);
                         }
                     }
@@ -326,7 +335,7 @@ public class ESEBConnector {
     }
 
     public void replyToMessage (RainbowESEBMessage msg, Object result) {
-        String repKey = (String )msg.getProperty (ESEBConstants.MSG_REPLY_KEY);
+        String repKey = (String) msg.getProperty (ESEBConstants.MSG_REPLY_KEY);
         if (repKey != null) {
             RainbowESEBMessage reply = createMessage (/*ChannelT.valueOf ((String )msg
                     .getProperty (ESEBConstants.MSG_CHANNEL_KEY))*/);
@@ -334,8 +343,7 @@ public class ESEBConnector {
             reply.setProperty (ESEBConstants.MSG_REPLY_KEY, repKey);
             try {
                 reply.setProperty (ESEBConstants.MSG_REPLY_VALUE, result);
-            }
-            catch (RainbowException e) {
+            } catch (RainbowException e) {
             }
             publish (reply);
         }
