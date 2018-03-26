b19f762a63bbc3756542a74a00791f34c970823c
Bradley Schmerl <schmerl@cs.cmu.edu>
2018-03-26 13:49:35 -0400

    Reorganized some badly copied files.

--- deployments/rainbow-brass/brass-p2-cp3/system/effectors/setSensor.sh
+++ deployments/rainbow-brass/brass-p2-cp3/system/effectors/setSensor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 . /opt/ros/kinetic/setup.bash
-. ~/catkin_ws/deve/setup.bash
+. ~/catkin_ws/devel/setup.bash
 
 sensor=$1
 if [ "$sensor" == "HEADLAMP" ]; then
-	if [ "$2" == "on" ]; then
+	if [ "$2" == "on" -o "$2" == "true" ]; then
 		rosservice call /mobile_base/headlamp True
 	else
 		rosservice call /mobile_base/headlamp False
