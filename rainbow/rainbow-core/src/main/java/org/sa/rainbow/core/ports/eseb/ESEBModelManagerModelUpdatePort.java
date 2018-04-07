b11b9228070433e2d77b871ed712c9d3aa1116b9
Bradley Schmerl <schmerl@cs.cmu.edu>
2018-04-07 12:48:22 -0400

    Changes to core to implement new GUI.

--- rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/ports/eseb/ESEBModelManagerModelUpdatePort.java
+++ rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/ports/eseb/ESEBModelManagerModelUpdatePort.java
@@ -23,10 +23,14 @@
  */
 package org.sa.rainbow.core.ports.eseb;
 
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.sa.rainbow.core.RainbowComponentT;
 import org.sa.rainbow.core.error.RainbowException;
-import org.sa.rainbow.core.error.RainbowModelException;
+import org.sa.rainbow.core.gauges.OperationRepresentation;
 import org.sa.rainbow.core.models.IModelInstance;
 import org.sa.rainbow.core.models.IModelUpdater;
 import org.sa.rainbow.core.models.ModelReference;
@@ -36,123 +40,128 @@ import org.sa.rainbow.core.ports.IRainbowReportingPort;
 import org.sa.rainbow.core.ports.eseb.ESEBConnector.ChannelT;
 import org.sa.rainbow.core.ports.eseb.ESEBConnector.IESEBListener;
 
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.LinkedList;
-import java.util.List;
+public class ESEBModelManagerModelUpdatePort extends AbstractESEBDisposablePort
+		implements ESEBConstants, IModelUSBusPort {
 
-public class ESEBModelManagerModelUpdatePort extends AbstractESEBDisposablePort implements ESEBConstants,
-IModelUSBusPort {
+	private final IRainbowReportingPort LOGGER = new ESEBMasterReportingPort();
 
-    private final IRainbowReportingPort LOGGER = new ESEBMasterReportingPort ();
+	private IModelUpdater m_mm;
 
-    private IModelUpdater m_mm;
+	public ESEBModelManagerModelUpdatePort(IModelUpdater mm) throws IOException {
+		super(ESEBProvider.getESEBClientHost(), ESEBProvider.getESEBClientPort(), ChannelT.MODEL_US);
+		m_mm = mm;
+		// Runs on master
+		getConnectionRole().addListener(new IESEBListener() {
 
-    public ESEBModelManagerModelUpdatePort (IModelUpdater mm) throws IOException {
-        super (ESEBProvider.getESEBClientHost (), ESEBProvider.getESEBClientPort (), ChannelT.MODEL_US);
-        m_mm = mm;
-        // Runs on master
-        getConnectionRole().addListener (new IESEBListener() {
+			@Override
+			public void receive(RainbowESEBMessage msg) {
+				String msgType = (String) msg.getProperty(ESEBConstants.MSG_TYPE_KEY);
+				if (ESEBConstants.MSG_TYPE_UPDATE_MODEL.equals(msgType)) {
+					String modelType = (String) msg.getProperty(MODEL_TYPE_KEY);
+					String modelName = (String) msg.getProperty(MODEL_NAME_KEY);
 
-            @Override
-            public void receive (RainbowESEBMessage msg) {
-                String msgType = (String )msg.getProperty (ESEBConstants.MSG_TYPE_KEY);
-                if (ESEBConstants.MSG_TYPE_UPDATE_MODEL.equals (msgType)) {
-                    String modelType = (String )msg.getProperty (MODEL_TYPE_KEY);
-                    String modelName = (String )msg.getProperty (MODEL_NAME_KEY);
+					List<String> params = new LinkedList<>();
+					params.add((String) msg.getProperty(COMMAND_TARGET_KEY));
+					int i = 0;
+					String p;
+					while ((p = (String) msg.getProperty(COMMAND_PARAMETER_KEY + i++)) != null) {
+						params.add(p);
+					}
 
-                    List<String> params = new LinkedList<> ();
-                    params.add ((String )msg.getProperty (COMMAND_TARGET_KEY));
-                    int i = 0;
-                    String p;
-                    while ((p = (String )msg.getProperty (COMMAND_PARAMETER_KEY + i++)) != null) {
-                        params.add (p);
-                    }
+					String commandName = (String) msg.getProperty(COMMAND_NAME_KEY);
+					try {
+						ModelReference mref = new ModelReference(modelName, modelType);
+						IModelInstance model = getModelInstance(mref);
+						if (model != null) {
+							IRainbowOperation command = model.getCommandFactory().generateCommand(commandName,
+									params.toArray(new String[params.size()]));
+							if (msg.hasProperty(COMMAND_ORIGIN)) {
+								command.setOrigin((String) msg.getProperty(COMMAND_ORIGIN));
+							}
+							updateModel(command);
+						} else {
+							params.remove(0);
+							IRainbowOperation command = new OperationRepresentation(commandName, mref,
+									(String) msg.getProperty(COMMAND_TARGET_KEY),
+									params.toArray(new String[params.size()]));
+							if (msg.hasProperty(COMMAND_ORIGIN)) {
+								command.setOrigin((String) msg.getProperty(COMMAND_ORIGIN));
+							}
+							updateModel(command);
+						}
 
-                    String commandName = (String )msg.getProperty (COMMAND_NAME_KEY);
-                    try {
-                        IModelInstance model = getModelInstance (new ModelReference (modelName, modelType));
-                        if (model != null) {
-                            IRainbowOperation command = model.getCommandFactory ().generateCommand (
-                                    commandName, params.toArray (new String[params.size ()]));
-                            if (msg.hasProperty (COMMAND_ORIGIN)) {
-                                command.setOrigin ((String )msg.getProperty (COMMAND_ORIGIN));
-                            }
-                            updateModel (command);
-                        }
-                        else
-                            throw new RainbowModelException (MessageFormat.format ("Could not find the referred model ''{0}'':''{1}''.", modelName,
-                                    modelType));
-                    }
-                    catch (Throwable e) {
-                        LOGGER.error (RainbowComponentT.MODEL, MessageFormat.format (
-                                "Could not form the command ''{0}'' from the ESEB message",
-                                commandName), e);
-                    }
-                }
-                else if ((ESEBConstants.MSG_TYPE_UPDATE_MODEL + "_multi").equals (msgType)) {
-                    int i = 0;
-                    IRainbowOperation cmd;
-                    List<IRainbowOperation> ops = new LinkedList<> ();
-                    do {
-                        cmd = ESEBCommandHelper.msgToCommand (msg, "_" + i + "_");
-                        if (cmd != null) {
-                            try {
-                                IModelInstance model = getModelInstance (cmd.getModelReference ());
-                                if (model != null) {
-                                    String[] params = new String[cmd.getParameters ().length + 1];
-                                    params[0] = cmd.getTarget ();
-                                    for (int j = 0; j < cmd.getParameters ().length; j++) {
-                                        params[j + 1] = cmd.getParameters ()[j];
-                                    }
-                                    IRainbowOperation command = model.getCommandFactory ().generateCommand (
-                                            cmd.getName (), params);
-                                    if (msg.hasProperty (COMMAND_ORIGIN)) {
-                                        command.setOrigin ((String )msg.getProperty (COMMAND_ORIGIN));
-                                    }
-                                    ops.add (command);
+					} catch (Throwable e) {
+						LOGGER.error(RainbowComponentT.MODEL, MessageFormat
+								.format("Could not form the command ''{0}'' from the ESEB message", commandName), e);
+					}
+				} else if ((ESEBConstants.MSG_TYPE_UPDATE_MODEL + "_multi").equals(msgType)) {
+					int i = 0;
+					IRainbowOperation cmd;
+					List<IRainbowOperation> ops = new LinkedList<>();
+					do {
+						cmd = ESEBCommandHelper.msgToCommand(msg, "_" + i + "_");
+						if (cmd != null) {
+							try {
+								IModelInstance model = getModelInstance(cmd.getModelReference());
+								if (model != null) {
+									String[] params = new String[cmd.getParameters().length + 1];
+									params[0] = cmd.getTarget();
+									for (int j = 0; j < cmd.getParameters().length; j++) {
+										params[j + 1] = cmd.getParameters()[j];
+									}
+									IRainbowOperation command = model.getCommandFactory().generateCommand(cmd.getName(),
+											params);
+									if (msg.hasProperty(COMMAND_ORIGIN)) {
+										command.setOrigin((String) msg.getProperty(COMMAND_ORIGIN));
+									}
+									ops.add(command);
 
-                                }
-                            }
-                            catch (Throwable e) {
-                                LOGGER.error (RainbowComponentT.MODEL, MessageFormat.format (
-                                        "Could not form the command ''{0}'' from the ESEB message", cmd.getName ()), e);
-                            }
-                        }
-                        i++;
-                    } while (cmd != null);
-                    boolean b = (Boolean )msg.getProperty (ESEBConstants.MSG_TRANSACTION);
-                    updateModel (ops, b);
+								} else {
+									IRainbowOperation command = new OperationRepresentation(cmd);
+									if (msg.hasProperty(COMMAND_ORIGIN)) {
+										command.setOrigin((String) msg.getProperty(COMMAND_ORIGIN));
+									}
+									ops.add(command);
+								}
+							} catch (Throwable e) {
+								LOGGER.error(RainbowComponentT.MODEL, MessageFormat.format(
+										"Could not form the command ''{0}'' from the ESEB message", cmd.getName()), e);
+							}
+						}
+						i++;
+					} while (cmd != null);
+					boolean b = (Boolean) msg.getProperty(ESEBConstants.MSG_TRANSACTION);
+					updateModel(ops, b);
 
-                }
-            }
-        });
+				}
+			}
+		});
 
-    }
+	}
 
-    @Override
-    public void updateModel (IRainbowOperation command) {
-        try {
-            m_mm.requestModelUpdate (command);
-        } catch (IllegalStateException | RainbowException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace ();
-        }
-    }
+	@Override
+	public void updateModel(IRainbowOperation command) {
+		try {
+			m_mm.requestModelUpdate(command);
+		} catch (IllegalStateException | RainbowException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void updateModel (List<IRainbowOperation> commands, boolean transaction) {
-        try {
-            m_mm.requestModelUpdate (commands, transaction);
-        } catch (IllegalStateException | RainbowException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace ();
-        }
-    }
+	@Override
+	public void updateModel(List<IRainbowOperation> commands, boolean transaction) {
+		try {
+			m_mm.requestModelUpdate(commands, transaction);
+		} catch (IllegalStateException | RainbowException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public IModelInstance getModelInstance (ModelReference modelRef) {
-        return m_mm.getModelInstance (modelRef);
-    }
+	@Override
+	public IModelInstance getModelInstance(ModelReference modelRef) {
+		return m_mm.getModelInstance(modelRef);
+	}
 
 }
