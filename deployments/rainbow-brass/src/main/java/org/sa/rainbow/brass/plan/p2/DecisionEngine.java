e348f8961dc81576dfe168de83188fb924a5355e
Bradley Schmerl <schmerl@cs.cmu.edu>
2018-10-03 10:40:26 -0400

    Comments

--- deployments/rainbow-brass/src/main/java/org/sa/rainbow/brass/plan/p2/DecisionEngine.java
+++ deployments/rainbow-brass/src/main/java/org/sa/rainbow/brass/plan/p2/DecisionEngine.java
@@ -147,10 +147,11 @@ public class DecisionEngine {
 			System.out.println("Prism initialized with constants: " + m_consts );
             log(m_consts);
             String result;
+            // For every candidate path
             for (List candidate_key : m_candidates.keySet() ){                           	
                 // We generate the policy for the priority QA
 				BRASSHttpConnector.instance(Phases.Phase2).reportStatus(DASPhase2StatusT.ADAPTING.name(), "Model checking path " + candidate_key.toString());
-
+				// Get the configuration tht is most likely to be successful on this path
             	result = PrismConnectorAPI.instance().modelCheckFromFileS (m_candidates.get(candidate_key), m_properties_file, m_candidates.get (candidate_key), m_priority_index, m_consts);
 				BRASSHttpConnector.instance(Phases.Phase2).reportStatus(DASPhase2StatusT.ADAPTING.name(), "  Finished first model check");
 
@@ -161,6 +162,7 @@ public class DecisionEngine {
                 ArrayList<String> reconfs = pp.getAllowedReconfigurations();
                 System.out.println("\t ** Allowed reconfigurations: "+ reconfs.toString()+"\n\n");
 
+                // Restrict reward model checking to only the most likely successful candidate
                 try {
                     appendToModel(m_candidates.get(candidate_key), "\n\n"+MapTranslator.generateReconfigurationConstraintModule(reconfs));
                 }catch (Exception e) {
@@ -168,6 +170,7 @@ public class DecisionEngine {
 
 				BRASSHttpConnector.instance(Phases.Phase2).reportStatus(DASPhase2StatusT.ADAPTING.name(), "  Starting second model check. PP=" + plan + ", AR=" + reconfs);
 
+				// Calculate the reward on this path for the selected configuration
                 result = PrismConnectorAPI.instance().modelCheckFromFileS (m_candidates.get(candidate_key), m_properties_file, m_candidates.get (candidate_key), -1, m_consts);
 				BRASSHttpConnector.instance(Phases.Phase2).reportStatus(DASPhase2StatusT.ADAPTING.name(), "  Result is " + result);
                 String[] results = result.split(",");
