53d06ab761c6dd6bf631b5bab5d8ff77847c9dcf
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-03-14 16:29:00 -0400

    mxLayout attempt

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/RainbowWindoe.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/RainbowWindoe.java
@@ -290,7 +290,7 @@ public class RainbowWindoe implements IRainbowGUI, IDisposable, IRainbowReportin
 			graph.getModel().endUpdate();
 		}
 		graph.setMaximumGraphBounds(new mxRectangle(400,400,WIDTH-400,HEIGHT-400));
-		mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+		mxCompactTreeLayout layout = new mxCompactTreeLayout(graph);
 //		layout.setLevelDistance(50);
 		layout.execute(graph.getDefaultParent());
 		for (Object c : cells) {
