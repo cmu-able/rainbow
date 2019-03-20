b06cb13cb348c0cdb88e2051c52fcd767b927c66
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-03-20 11:44:12 -0400

    Size improvements

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/widgets/MeterPanel.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/widgets/MeterPanel.java
@@ -4,11 +4,13 @@ import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JPanel;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.DialShape;
 import org.jfree.chart.plot.MeterInterval;
 import org.jfree.chart.plot.MeterPlot;
 import org.jfree.data.Range;
@@ -37,7 +39,7 @@ public class MeterPanel extends JPanel implements ICommandUpdate {
 		m_chart = createChart();
 		ChartPanel cp = new ChartPanel(m_chart);
 		cp.setMinimumSize(new Dimension(50, 50));
-		cp.setSize(50,50);
+		cp.setSize(50, 50);
 		add(cp, BorderLayout.CENTER);
 	}
 
@@ -48,6 +50,19 @@ public class MeterPanel extends JPanel implements ICommandUpdate {
 			meter.addInterval(new MeterInterval("High", new Range(m_redZone, m_upper), Color.red, new BasicStroke(4.0f),
 					new Color(255, 0, 0, 128)));
 		}
+		meter.setNeedlePaint(Color.darkGray);
+		meter.setDialBackgroundPaint(Color.white);
+		meter.setDialOutlinePaint(Color.black);
+		meter.setDialShape(DialShape.CHORD);
+		meter.setMeterAngle(180);
+		meter.setTickLabelsVisible(false);
+//		    meter.setTickLabelFont(new Font("Arial", 1, 12));
+		meter.setTickLabelPaint(Color.black);
+//		    meter.setTickSize(5D);
+		meter.setTickPaint(Color.gray);
+		meter.setValuePaint(Color.black);
+//		    meter.set
+//		    meter.setValueFont(new Font("Arial", 1, 14));
 		JFreeChart chart = new JFreeChart(null, JFreeChart.DEFAULT_TITLE_FONT, meter, false);
 		return chart;
 	}
