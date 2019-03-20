33ee8520576ef1cb6c4781f59af5f49afc5c1163
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-03-20 12:45:26 -0400

    Size improvements

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/arch/DynamicDesktopIconUI.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/arch/DynamicDesktopIconUI.java
@@ -32,7 +32,7 @@ public class DynamicDesktopIconUI extends BasicDesktopIconUI {
 		desktopIcon.setOpaque(false);
 		desktopIcon.setLayout(new BorderLayout());
 		desktopIcon.add(series, BorderLayout.CENTER);
-		JLabel label = new JLabel(MessageFormat.format("<html><div width=%d>%ss</div></html>", series.getPreferredSize().width, title), SwingConstants.CENTER);
+		JLabel label = new JLabel(MessageFormat.format("<html><div width=%d>%s</div></html>", series.getPreferredSize().width, title), SwingConstants.CENTER);
 		label.setFont(new Font(label.getFont().getFontName(), label.getFont().getStyle(), 8));
 
 		desktopIcon.add(label, BorderLayout.SOUTH);
