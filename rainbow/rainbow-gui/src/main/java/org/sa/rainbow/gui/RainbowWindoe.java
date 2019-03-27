3744b1599eacf2dcc71c8f81a0697d4217191aec
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-03-27 14:54:17 -0400

    Size probelms.

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/RainbowWindoe.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/RainbowWindoe.java
@@ -1035,7 +1035,8 @@ public class RainbowWindoe extends RainbowWindow
 			JInternalFrame frame = new JInternalFrame(a.id(), true, false, true);
 			frame.setVisible(true);
 			frame.add(uiComp);
-//			frame.setSize(uiComp.getPreferredSize());
+			Dimension pf = uiComp.getPreferredSize();
+			frame.setSize(pf.width, pf.height+25);
 			frame.addPropertyChangeListener(e -> {
 				System.out.println("Selected " + a.id());
 				if ("selection".equals(e.getPropertyName())) {
