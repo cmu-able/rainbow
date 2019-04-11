abb50417b308e5b737c7e0ef201c4ed693acdaa1
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-04-11 17:08:42 -0400

    Model detail panel.

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/widgets/ModelErrorRenderer.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/widgets/ModelErrorRenderer.java
@@ -12,13 +12,23 @@ import org.sa.rainbow.gui.RainbowWindow;
 
 public class ModelErrorRenderer extends DefaultTableCellRenderer implements TableCellRenderer {
 	private static Color m_red = RainbowWindow.bleach(Color.red, 0.75);
+	
+	public static interface IErrorChecker {
+		boolean hasError(TableModel tm, int row);
+	}
+
+	private IErrorChecker m_errorCheck;
+	
+	public ModelErrorRenderer(IErrorChecker check) {
+		m_errorCheck = check;
+	}
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
 		Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 		TableModel tm = table.getModel();
-		if (!"".equals(tm.getValueAt(row, 4))) {
+		if (m_errorCheck.hasError(tm, row)) {
 			c.setBackground(m_red);
 		}
 		else if (c.getBackground() == m_red) c.setBackground(Color.WHITE);
