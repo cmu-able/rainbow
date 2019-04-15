552d53a150f9abc1dd20a095aa507c977a4d5cce
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-04-15 07:13:57 -0400

    Implemented power model

--- deployments/rainbow-brass/src/main/java/org/sa/rainbow/brass/p3_cp1/model/power/PowerModelCommandFactory.java
+++ deployments/rainbow-brass/src/main/java/org/sa/rainbow/brass/p3_cp1/model/power/PowerModelCommandFactory.java
@@ -1,13 +1,22 @@
 package org.sa.rainbow.brass.p3_cp1.model.power;
 
+import java.io.InputStream;
+
 import org.sa.rainbow.brass.confsynthesis.SimpleConfigurationStore;
 import org.sa.rainbow.core.error.RainbowModelException;
-import org.sa.rainbow.core.models.IModelInstance;
+import org.sa.rainbow.core.models.ModelsManager;
 import org.sa.rainbow.core.models.commands.AbstractSaveModelCmd;
 import org.sa.rainbow.core.models.commands.ModelCommandFactory;
 
 public class PowerModelCommandFactory extends ModelCommandFactory<SimpleConfigurationStore> {
 
+	public static LoadPowerModelCmd loadCommand(ModelsManager mm,
+			String modelName,
+			InputStream stream,
+			String source) {
+		return new LoadPowerModelCmd(mm, modelName, stream, source);
+	}
+	
 	public PowerModelCommandFactory(
 			CP1PowerModelInstance model) {
 		super(CP1PowerModelInstance.class, model);
@@ -15,14 +24,16 @@ public class PowerModelCommandFactory extends ModelCommandFactory<SimpleConfigur
 
 	@Override
 	protected void fillInCommandMap() {
-		// TODO Auto-generated method stub
-		
+		m_commandMap.put("updatePowerModel".toLowerCase(), UpdatePowerModelCmd.class);
 	}
 
 	@Override
 	public AbstractSaveModelCmd<SimpleConfigurationStore> saveCommand(String location) throws RainbowModelException {
-		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public UpdatePowerModelCmd updatePowerModelCmd(String newFileName) {
+		return new UpdatePowerModelCmd((CP1PowerModelInstance )m_modelInstance,"",newFileName);
 	}
 
 }
