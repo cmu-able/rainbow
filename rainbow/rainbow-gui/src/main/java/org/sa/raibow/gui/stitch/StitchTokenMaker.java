e837178c5a6faadb70df6861c6312b9beef41f60
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-04-24 14:28:42 -0400

    New lexer

--- rainbow/rainbow-gui/src/main/java/org/sa/raibow/gui/stitch/StitchTokenMaker.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/raibow/gui/stitch/StitchTokenMaker.java
@@ -46,22 +46,20 @@ public class StitchTokenMaker extends AbstractTokenMaker {
 			new TokenInfo("record", Token.DATA_TYPE), new TokenInfo("enum", Token.DATA_TYPE),
 			new TokenInfo(";", Token.SEPARATOR), new TokenInfo("@", Token.OPERATOR),
 			new TokenInfo("<=", Token.OPERATOR), new TokenInfo(">=", Token.OPERATOR),
-			new TokenInfo("<", Token.OPERATOR), new TokenInfo(">", Token.OPERATOR),
-			new TokenInfo("+", Token.OPERATOR), new TokenInfo("*", Token.OPERATOR),
-			new TokenInfo("/", Token.OPERATOR), new TokenInfo("'", Token.OPERATOR),
+			new TokenInfo("<", Token.OPERATOR), new TokenInfo(">", Token.OPERATOR), new TokenInfo("+", Token.OPERATOR),
+			new TokenInfo("*", Token.OPERATOR), new TokenInfo("/", Token.OPERATOR), new TokenInfo("'", Token.OPERATOR),
 			new TokenInfo("...", Token.OPERATOR), new TokenInfo("==", Token.OPERATOR),
-			new TokenInfo("|", Token.OPERATOR), new TokenInfo("#", Token.OPERATOR),
-			new TokenInfo("=", Token.OPERATOR),new TokenInfo("+=", Token.OPERATOR),
-			new TokenInfo("-=", Token.OPERATOR),new TokenInfo("*=", Token.OPERATOR),
-			new TokenInfo("/=", Token.OPERATOR),new TokenInfo("%=", Token.OPERATOR),
-			new TokenInfo("%", Token.OPERATOR),new TokenInfo("++", Token.OPERATOR),
-			new TokenInfo("--", Token.OPERATOR),new TokenInfo("!", Token.OPERATOR),
-			new TokenInfo("->", Token.OPERATOR),new TokenInfo("<->", Token.OPERATOR),
-			new TokenInfo(",", Token.SEPARATOR),new TokenInfo("{", Token.SEPARATOR),
-			new TokenInfo("}", Token.SEPARATOR), new TokenInfo("(", Token.SEPARATOR),
-			new TokenInfo(")", Token.SEPARATOR),new TokenInfo("[", Token.SEPARATOR),
-			new TokenInfo("]", Token.SEPARATOR), new TokenInfo(".", Token.SEPARATOR),
-			new TokenInfo(":", Token.SEPARATOR)};
+			new TokenInfo("|", Token.OPERATOR), new TokenInfo("#", Token.OPERATOR), new TokenInfo("=", Token.OPERATOR),
+			new TokenInfo("+=", Token.OPERATOR), new TokenInfo("-=", Token.OPERATOR),
+			new TokenInfo("*=", Token.OPERATOR), new TokenInfo("/=", Token.OPERATOR),
+			new TokenInfo("%=", Token.OPERATOR), new TokenInfo("%", Token.OPERATOR),
+			new TokenInfo("++", Token.OPERATOR), new TokenInfo("--", Token.OPERATOR),
+			new TokenInfo("!", Token.OPERATOR), new TokenInfo("->", Token.OPERATOR),
+			new TokenInfo("<->", Token.OPERATOR), new TokenInfo(",", Token.SEPARATOR),
+			new TokenInfo("{", Token.SEPARATOR), new TokenInfo("}", Token.SEPARATOR),
+			new TokenInfo("(", Token.SEPARATOR), new TokenInfo(")", Token.SEPARATOR),
+			new TokenInfo("[", Token.SEPARATOR), new TokenInfo("]", Token.SEPARATOR),
+			new TokenInfo(".", Token.SEPARATOR), new TokenInfo(":", Token.SEPARATOR) };
 	private int currentTokenStart;
 	private int currentTokenType;
 
@@ -96,167 +94,231 @@ public class StitchTokenMaker extends AbstractTokenMaker {
 		currentTokenType = startTokenType;
 
 		for (int i = offset; i < end; i++) {
+			if (currentTokenType == Token.LITERAL_STRING_DOUBLE_QUOTE && array[i] != '"') {
+				continue;
+			}
 
-			char c = array[i];
-
-			switch (currentTokenType) {
-
-			case Token.NULL:
-
-				currentTokenStart = i; // Starting a new token here.
-
-				switch (c) {
-
-				case ' ':
-				case '\t':
-					currentTokenType = Token.WHITESPACE;
-					break;
-
-				case '"':
-					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
-					break;
-
-				case '#':
-					currentTokenType = Token.COMMENT_EOL;
-					break;
-
-				default:
-					if (RSyntaxUtilities.isDigit(c)) {
-						currentTokenType = Token.LITERAL_NUMBER_DECIMAL_INT;
-						break;
-					} else if (RSyntaxUtilities.isLetter(c) || c == '/' || c == '_') {
-						currentTokenType = Token.IDENTIFIER;
-						break;
-					}
-
-					// Anything not currently handled - mark as an identifier
-					currentTokenType = Token.IDENTIFIER;
-					break;
-
-				} // End of switch (c).
-
-				break;
-
-			case Token.WHITESPACE:
-
-				switch (c) {
-
-				case ' ':
-				case '\t':
-					break; // Still whitespace.
-
-				case '"':
-					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
-					break;
-
-				case '#':
-					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.COMMENT_EOL;
-					break;
-
-				default: // Add the whitespace token and start anew.
-
-					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-
-					if (RSyntaxUtilities.isDigit(c)) {
-						currentTokenType = Token.LITERAL_NUMBER_DECIMAL_INT;
-						break;
-					} else if (RSyntaxUtilities.isLetter(c) || c == '/' || c == '_') {
-						currentTokenType = Token.IDENTIFIER;
-						break;
-					}
-
-					// Anything not currently handled - mark as identifier
-					currentTokenType = Token.IDENTIFIER;
-
-				} // End of switch (c).
-
-				break;
-
-			default: // Should never happen
-			case Token.IDENTIFIER:
-
-				switch (c) {
-
-				case ' ':
-				case '\t':
-					addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.WHITESPACE;
-					break;
-
-				case '"':
-					addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
-					break;
-
-				default:
-					if (RSyntaxUtilities.isLetterOrDigit(c) || c == '/' || c == '_') {
-						break; // Still an identifier of some type.
-					}
-					// Otherwise, we're still an identifier (?).
-
-				} // End of switch (c).
-
-				break;
-
-			case Token.LITERAL_NUMBER_DECIMAL_INT:
-
-				switch (c) {
-
-				case ' ':
-				case '\t':
-					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
-							newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.WHITESPACE;
-					break;
-
-				case '"':
-					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
-							newStartOffset + currentTokenStart);
-					currentTokenStart = i;
-					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
-					break;
-
-				default:
-
-					if (RSyntaxUtilities.isDigit(c)) {
-						break; // Still a literal number.
-					}
-
-					// Otherwise, remember this was a number and start over.
-					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
-							newStartOffset + currentTokenStart);
-					i--;
-					currentTokenType = Token.NULL;
-
-				} // End of switch (c).
-
-				break;
-
-			case Token.COMMENT_EOL:
-				i = end - 1;
-				addToken(text, currentTokenStart, i, currentTokenType, newStartOffset + currentTokenStart);
-				// We need to set token type to null so at the bottom we don't add one more
-				// token.
-				currentTokenType = Token.NULL;
-				break;
-
-			case Token.LITERAL_STRING_DOUBLE_QUOTE:
-				if (c == '"') {
-					addToken(text, currentTokenStart, i, Token.LITERAL_STRING_DOUBLE_QUOTE,
-							newStartOffset + currentTokenStart);
-					currentTokenType = Token.NULL;
+			int newTokenType = wordsToHighlight.get(array, currentTokenStart, i);
+			boolean skip = false;
+			if (newTokenType == Token.RESERVED_WORD || newTokenType == Token.RESERVED_WORD_2) {
+				// Lookahead to make sure it isn't an identifier
+				if (i + 1 < end && (Character.isLetter(array[i + 1]) || '_' == array[i + 1])) {
+					newTokenType = Token.IDENTIFIER;
 				}
-				break;
+				else {
+					currentTokenType = newTokenType;
+				}
+			} else {
+				char ch = array[i];
+				if (ch == ' ' || ch == '\t') {
+					newTokenType = Token.WHITESPACE;
+				} else if (ch == '"') {
+					if (currentTokenType != Token.LITERAL_STRING_DOUBLE_QUOTE) {
+						newTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
+					} else {
+						addToken(text, currentTokenStart, i, Token.LITERAL_STRING_DOUBLE_QUOTE,
+								newStartOffset + currentTokenStart);
+						skip = true;
+						currentTokenStart = i + 1;
+						currentTokenType = Token.NULL;
+					}
+				} else if (Character.isDigit(ch)) {
+					if (currentTokenType == Token.IDENTIFIER)
+						newTokenType = Token.IDENTIFIER;
+					else
+						newTokenType = (currentTokenType == Token.LITERAL_NUMBER_FLOAT) ? Token.LITERAL_NUMBER_FLOAT
+								: Token.LITERAL_NUMBER_DECIMAL_INT;
+				} else if ('.' == ch && currentTokenType == Token.LITERAL_NUMBER_DECIMAL_INT) {
+					currentTokenType = newTokenType = Token.LITERAL_NUMBER_FLOAT;
+				} else if (Character.isLetter(ch)) {
+					newTokenType = Token.IDENTIFIER;
+				} else if ('_' == ch) {
+					if (currentTokenType == Token.IDENTIFIER)
+						newTokenType = Token.IDENTIFIER;
+					else
+						newTokenType = Token.NULL;
+				} else if ((newTokenType = wordsToHighlight.get(array, i, i)) != -1) {
 
-			} // End of switch (currentTokenType).
+				}
+			}
+			if (newTokenType == Token.NULL || newTokenType == -1 || newTokenType == Token.WHITESPACE) {
+				if (currentTokenType == Token.NULL || currentTokenType == Token.WHITESPACE || currentTokenType == -1) {
+					skip = true;
+					currentTokenStart = i;
+				}
+			}
+			if (!skip && newTokenType != currentTokenType) {
+				switch (currentTokenType) {
+				case Token.NULL:
+				case -1:
+					break;
+				default:
+					addToken(text, currentTokenStart, i - 1, currentTokenType, newStartOffset + currentTokenStart);
+				}
+				currentTokenStart = i;
+				currentTokenType = newTokenType;
+
+			}
+//			char c = array[i];
+//
+//			switch (currentTokenType) {
+//
+//			case Token.NULL:
+//
+//				currentTokenStart = i; // Starting a new token here.
+//				
+//				
+//
+//				switch (c) {
+//
+//				case ' ':
+//				case '\t':
+//					currentTokenType = Token.WHITESPACE;
+//					break;
+//
+//				case '"':
+//					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
+//					break;
+//
+//				
+//
+//				default:
+//					if (RSyntaxUtilities.isDigit(c)) {
+//						currentTokenType = Token.LITERAL_NUMBER_DECIMAL_INT;
+//						break;
+//					} else if (RSyntaxUtilities.isLetter(c) || c == '/' || c == '_') {
+//						currentTokenType = Token.IDENTIFIER;
+//						break;
+//					}
+//
+//					// Anything not currently handled - mark as an identifier
+//					currentTokenType = Token.IDENTIFIER;
+//					break;
+//
+//				} // End of switch (c).
+//
+//				break;
+//
+//			case Token.WHITESPACE:
+//
+//				switch (c) {
+//
+//				case ' ':
+//				case '\t':
+//					break; // Still whitespace.
+//
+//				case '"':
+//					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
+//					break;
+//
+//				case '#':
+//					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.COMMENT_EOL;
+//					break;
+//
+//				default: // Add the whitespace token and start anew.
+//
+//					addToken(text, currentTokenStart, i - 1, Token.WHITESPACE, newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//
+//					if (RSyntaxUtilities.isDigit(c)) {
+//						currentTokenType = Token.LITERAL_NUMBER_DECIMAL_INT;
+//						break;
+//					} else if (RSyntaxUtilities.isLetter(c) || c == '/' || c == '_') {
+//						currentTokenType = Token.IDENTIFIER;
+//						break;
+//					}
+//
+//					// Anything not currently handled - mark as identifier
+//					currentTokenType = Token.IDENTIFIER;
+//
+//				} // End of switch (c).
+//
+//				break;
+//
+//			default: // Should never happen
+//			case Token.IDENTIFIER:
+//
+//				switch (c) {
+//
+//				case ' ':
+//				case '\t':
+//					addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.WHITESPACE;
+//					break;
+//
+//				case '"':
+//					addToken(text, currentTokenStart, i - 1, Token.IDENTIFIER, newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
+//					break;
+//
+//				default:
+//					if (RSyntaxUtilities.isLetterOrDigit(c) || c == '/' || c == '_') {
+//						break; // Still an identifier of some type.
+//					}
+//					// Otherwise, we're still an identifier (?).
+//
+//				} // End of switch (c).
+//
+//				break;
+//
+//			case Token.LITERAL_NUMBER_DECIMAL_INT:
+//
+//				switch (c) {
+//
+//				case ' ':
+//				case '\t':
+//					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
+//							newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.WHITESPACE;
+//					break;
+//
+//				case '"':
+//					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
+//							newStartOffset + currentTokenStart);
+//					currentTokenStart = i;
+//					currentTokenType = Token.LITERAL_STRING_DOUBLE_QUOTE;
+//					break;
+//
+//				default:
+//
+//					if (RSyntaxUtilities.isDigit(c)) {
+//						break; // Still a literal number.
+//					}
+//
+//					// Otherwise, remember this was a number and start over.
+//					addToken(text, currentTokenStart, i - 1, Token.LITERAL_NUMBER_DECIMAL_INT,
+//							newStartOffset + currentTokenStart);
+//					i--;
+//					currentTokenType = Token.NULL;
+//
+//				} // End of switch (c).
+//
+//				break;
+//
+//			case Token.COMMENT_EOL:
+//				i = end - 1;
+//				addToken(text, currentTokenStart, i, currentTokenType, newStartOffset + currentTokenStart);
+//				// We need to set token type to null so at the bottom we don't add one more
+//				// token.
+//				currentTokenType = Token.NULL;
+//				break;
+//
+//			case Token.LITERAL_STRING_DOUBLE_QUOTE:
+//				if (c == '"') {
+//					addToken(text, currentTokenStart, i, Token.LITERAL_STRING_DOUBLE_QUOTE,
+//							newStartOffset + currentTokenStart);
+//					currentTokenType = Token.NULL;
+//				}
+//				break;
+//
+//			} // End of switch (currentTokenType).
 
 		} // End of for (int i=offset; i<end; i++).
 
