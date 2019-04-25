333fd2c73bb23ef38e6b527bcfe7aceed1e83479
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-04-25 11:10:03 -0400

    Size tweak in UI

--- rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/arch/elements/UtilityModelPane.java
+++ rainbow/rainbow-gui/src/main/java/org/sa/rainbow/gui/arch/elements/UtilityModelPane.java
@@ -33,7 +33,7 @@ public class UtilityModelPane extends JPanel {
 	 */
 	public UtilityModelPane() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 150, 200, 150, 0, 0, 0 };
+		gridBagLayout.columnWidths = new int[] { 150, 200, 150, 0, 200, 0 };
 		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
