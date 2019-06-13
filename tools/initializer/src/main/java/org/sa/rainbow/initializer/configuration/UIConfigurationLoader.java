558ab4f1b632a345545687c1235e2570fed2c06a
jumoni <liwenf@andrew.cmu.edu>
2019-06-13 16:54:38 -0400

    add default config

--- tools/initializer/src/main/java/org/sa/rainbow/initializer/configuration/UIConfigurationLoader.java
+++ tools/initializer/src/main/java/org/sa/rainbow/initializer/configuration/UIConfigurationLoader.java
@@ -3,7 +3,9 @@ package org.sa.rainbow.initializer.configuration;
 import freemarker.template.Template;
 import org.sa.rainbow.initializer.models.TemplateSet;
 import org.sa.rainbow.initializer.models.Variable;
+import org.sa.rainbow.initializer.template.FileTemplateSetLoader;
 import org.sa.rainbow.initializer.template.InvalidMetadataException;
+import org.sa.rainbow.initializer.template.TemplateSetLoader;
 import org.sa.rainbow.initializer.template.models.Metadata;
 import org.yaml.snakeyaml.Yaml;
 
@@ -17,6 +19,55 @@ public class UIConfigurationLoader {
      * The default configuration file.
      */
     protected static final String METADATA_FILENAME = "metadata.yml";
+
+    /**
+     * A map of key values for the required variables.
+     */
+    private final Map<String, String> defaultConfig;
+
+    /**
+     * A set of known variable names.
+     * Please note that we may not want to use defaultConfig directly
+     * because some variables may be required but have no default values.
+     */
+    private final Set<String> variableNames;
+
+    public UIConfigurationLoader() {
+        this.defaultConfig = new HashMap<>();
+        this.variableNames = new HashSet<>();
+    }
+
+    public void defaultConfiguration(List<Variable> variables) {
+        for (Variable variable : variables) {
+            variableNames.add(variable.getName());
+            defaultConfig.put(variable.getName(), variable.getValue());
+        }
+    }
+
+    /**
+     * Loads configuration setting from a file (java .properties file)
+     *
+     * @param file the properties file to load.
+     * @throws IOException if the file cannot be loaded as properties file.
+     */
+    public Map<String, String> fileConfiguration(File file) throws IOException {
+        Map<String, String> config = new HashMap<>(defaultConfig);
+        try (InputStream input = new FileInputStream(file)) {
+            Properties prop = new Properties();
+            prop.load(input);
+            Set<Map.Entry<Object, Object>> entries = prop.entrySet();
+            for (Map.Entry<Object, Object> entry : entries) {
+                String name = (String) entry.getKey();
+                String value = (String) entry.getValue();
+                // The name was not known as the name of a variable.
+                if (!variableNames.contains(name)) {
+                    throw new InvalidVariableException("unknown variable: " + name);
+                }
+                config.put(name, value);
+            }
+        }
+        return config;
+    }
 
     /**
      * Loads the metadata from a reader.
@@ -85,17 +136,19 @@ public class UIConfigurationLoader {
         // if answer is yes, we will generate configuration using default values.
         if(answer.equals("yes") || answer.equals("y")) {
             System.out.println("We will use the default values to initialize.");
-            // TODO: need other operations?
+            TemplateSetLoader loader = new FileTemplateSetLoader(new File("templates"));
+            TemplateSet templateSet = loader.load();
+            defaultConfiguration(templateSet.getVariables());
         }
         // if answer is no, we will call an editor for users
         else if(answer.equals("no") || answer.equals("n")){
             System.out.println("Please set your own configurations and run again.");
             try {
                 Desktop desktop = Desktop.getDesktop();
-                File file = new File("config.properties");
+                File file = new File("config.yml");
                 file.createNewFile();
                 desktop.open(file);
-                // TODO: need other operations?
+                // fileConfiguration(file);
             } catch (Exception e) {
                 e.printStackTrace(System.out);
             }
@@ -104,6 +157,14 @@ public class UIConfigurationLoader {
             System.out.println("Abort!");
         }
 
+    }
+
+    public Map<String, String> getDefaultConfig() {
+        return defaultConfig;
+    }
+
+    public Set<String> getVariableNames() {
+        return variableNames;
     }
 
     /**
