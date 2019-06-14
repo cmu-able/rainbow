a7fa26f962fd999c9caeaff1ddae0b44c76ff4ec
lennyzhang <yunhengz@andrew.cmu.edu>
2019-06-13 22:32:15 -0400

    refactored main and added tests

--- tools/initializer/src/main/java/org/sa/rainbow/initializer/Main.java
+++ tools/initializer/src/main/java/org/sa/rainbow/initializer/Main.java
@@ -1,50 +1,26 @@
 package org.sa.rainbow.initializer;
 
 import org.apache.commons.cli.*;
-import org.apache.commons.validator.routines.UrlValidator;
 import org.sa.rainbow.initializer.configuration.ConfigurationLoader;
 import org.sa.rainbow.initializer.models.TemplateSet;
+import org.sa.rainbow.initializer.parser.OptionParser;
 import org.sa.rainbow.initializer.scaffolder.Scaffolder;
 import org.sa.rainbow.initializer.template.FileTemplateSetLoader;
 import org.sa.rainbow.initializer.template.TemplateSetLoader;
 
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-
 
 /**
  * The entry point of initializer.
  */
 public class Main {
+
     public static void main(String[] args) throws Exception {
 
-        // initializer, as a command line tool, could take in two flags, -t for template and -c for config
-        Options options = new Options();
-        Option option_template = Option.builder("t")
-                .argName("path_or_uri_to_template")
-                .hasArg()
-                .desc("load template from either path to local file or uri")
-                .longOpt("template")
-                .build();
-        Option option_config = Option.builder("c")
-                .argName("path_or_uri_to_config")
-                .hasArg()
-                .desc("load configuration from either path to local file or uri")
-                .longOpt("config")
-                .build();
-        options.addOption(option_template);
-        options.addOption(option_config);
-
-        // help info can be displayed when using the -h or --help flag
-        Option option_help = Option.builder("h")
-                .longOpt("help")
-                .desc("help menu")
-                .build();
-        options.addOption(option_help);
-
+        OptionParser optionParser = new OptionParser();
+        Options options = optionParser.getOptions();
         TemplateSet templateSet = null;
         try {
 
@@ -58,83 +34,21 @@ public class Main {
                 formatter.printHelp("rainbow_initializer", options, true);
             }
 
-            UrlValidator urlValidator = new UrlValidator();
-
             // if user provides a template flag, parse template path
             if (cmd.hasOption("t")) {
-
-                // if path is a valid uri, read and parse it
-                if (urlValidator.isValid(cmd.getOptionValue("t"))) {
-
-                    // TODO: read and parse template as URI here
-                    System.out.print("Valid template uri: ");
-                    System.out.println(cmd.getOptionValue("t"));
-
-                    // otherwise, check if it is a valid local file
-                } else {
-
-                    // if path cannot be instantiated into a Java Path object, print error and return
-                    try {
-                        Path file = Paths.get(cmd.getOptionValue("t"));
-                    } catch (InvalidPathException e) {
-                        System.out.println("Parse error: the input template path is invalid.");
-                        return;
-                    }
-
-                    // if path does not point to a local file, print error and return
-                    Path file = Paths.get(cmd.getOptionValue("t"));
-                    if (!Files.isDirectory(file)) {
-                        System.out.println("Parse error: the input template path does not point to a local file.");
-                        return;
-                    }
-
-                    // TODO: read and parse template as local file here
-                    System.out.print("Valid template path: ");
-                    System.out.println(cmd.getOptionValue("t"));
-
-                    TemplateSetLoader templateSetLoader = new FileTemplateSetLoader(file.toFile());
-                    templateSet = templateSetLoader.load();
-                }
+                Path file = optionParser.handleTemplateOption(cmd);
+                TemplateSetLoader templateSetLoader = new FileTemplateSetLoader(file.toFile());
+                templateSet = templateSetLoader.load();
             }
 
             // if user provides a config flag, parse the config path
             if (cmd.hasOption("c")) {
-
-                // if path is a valid uri, read and parse it
-                if (urlValidator.isValid(cmd.getOptionValue("c"))) {
-
-                    // TODO: read and parse template as URI here
-                    System.out.print("Valid config uri: ");
-                    System.out.println(cmd.getOptionValue("c"));
-
-                    // otherwise, check if it is a valid local file
-                } else {
-
-                    // if path cannot be instantiated into a Java Path object, print error and return
-                    try {
-                        Path file = Paths.get(cmd.getOptionValue("c"));
-                    } catch (InvalidPathException e) {
-                        System.out.println("Parse error: the input configuration path is invalid.");
-                        return;
-                    }
-
-                    // if path does not point to a local file, print error and return
-                    Path file = Paths.get(cmd.getOptionValue("c"));
-                    if (!Files.isRegularFile(file) || !Files.isReadable(file)) {
-                        System.out.println("Parse error: the input configuration path does not point to a local file.");
-                        return;
-                    }
-
-                    // TODO: read and parse configuration as local file here
-                    System.out.print("Valid config path: ");
-                    System.out.println(cmd.getOptionValue("c"));
-
-                    ConfigurationLoader configurationLoader = new ConfigurationLoader(templateSet.getVariables());
-                    Map<String, Object> configuration = configurationLoader.loadConfiguration(file.toFile());
-                    Scaffolder scaffolder = new Scaffolder(templateSet, configuration);
-                    scaffolder.setBaseDirectory(Paths.get(".")); //TODO: make it a parameter
-                    scaffolder.scaffold();
-                }
+                Path file = optionParser.handleConfigOption(cmd);
+                ConfigurationLoader configurationLoader = new ConfigurationLoader(templateSet.getVariables());
+                Map<String, Object> configuration = configurationLoader.loadConfiguration(file.toFile());
+                Scaffolder scaffolder = new Scaffolder(templateSet, configuration);
+                scaffolder.setBaseDirectory(Paths.get("."));
+                scaffolder.scaffold();
             }
         }
 
---
+++ tools/initializer/src/main/java/org/sa/rainbow/initializer/parser/OptionParser.java
@@ -0,0 +1,110 @@
+package org.sa.rainbow.initializer.parser;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Parser that parses the option flags of rainbow initializer
+ */
+public class OptionParser {
+
+    private Options options;
+
+    /**
+     * Constructor for an option parser that takes -t, -c and -h flags.
+     */
+    public OptionParser () {
+        // initializer, as a command line tool, could take in two flags, -t for template and -c for config
+        options = new Options();
+        Option option_template = Option.builder("t")
+                .argName("path_to_template")
+                .hasArg()
+                .desc("load template from path to local directory")
+                .longOpt("template")
+                .build();
+        Option option_config = Option.builder("c")
+                .argName("path_to_config")
+                .hasArg()
+                .desc("load configuration from path to local file")
+                .longOpt("config")
+                .build();
+        options.addOption(option_template);
+        options.addOption(option_config);
+
+        // help info can be displayed when using the -h or --help flag
+        Option option_help = Option.builder("h")
+                .longOpt("help")
+                .desc("help menu")
+                .build();
+        options.addOption(option_help);
+    }
+
+    /**
+     * Getter for options.
+     *
+     * @return all options created by the OptionParser
+     */
+    public Options getOptions() {
+        return options;
+    }
+
+    /**
+     * Handler of the template flag.
+     *
+     * @param cmd the commnd line input
+     * @return path to the directory containing templates
+     */
+    public Path handleTemplateOption(CommandLine cmd) throws InvalidPathException {
+        // if path cannot be instantiated into a Java Path object, print error and return
+        try {
+            Path file = Paths.get(cmd.getOptionValue("t"));
+        } catch (InvalidPathException e) {
+            System.out.println("Parse error: the input template path is invalid.");
+        }
+
+        // if path does not point to a local file, print error and return
+        Path file = Paths.get(cmd.getOptionValue("t"));
+        if (!Files.isDirectory(file)) {
+            System.out.println("Parse error: the input template path does not point to a local directory.");
+            return file;
+        }
+
+        System.out.print("Valid template path: ");
+        System.out.println(cmd.getOptionValue("t"));
+
+        return file;
+    }
+
+    /**
+     * Handler of the configuration flag.
+     *
+     * @param cmd command line input
+     * @return path to the file containing configurations
+     */
+    public Path handleConfigOption(CommandLine cmd) throws InvalidPathException{
+        // if path cannot be instantiated into a Java Path object, print error and return
+        try {
+            Path file = Paths.get(cmd.getOptionValue("c"));
+        } catch (InvalidPathException e) {
+            System.out.println("Parse error: the input configuration path is invalid.");
+        }
+
+        // if path does not point to a local file, print error and return
+        Path file = Paths.get(cmd.getOptionValue("c"));
+        if (!Files.isRegularFile(file)) {
+            System.out.println("Parse error: the input configuration path does not point to a local file.");
+            return file;
+        }
+
+        System.out.print("Valid config path: ");
+        System.out.println(cmd.getOptionValue("c"));
+
+        return file;
+    }
+}
