7faaee323cb67955b8c39e2b328806a18bec453c
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-07-02 16:48:29 -0400

    More scrolling experiments.

--- rainbow/rainbow-stitch/src/main/java/org/sa/rainbow/stitch/gui/executor/StrategyExecutionPanel.java
+++ rainbow/rainbow-stitch/src/main/java/org/sa/rainbow/stitch/gui/executor/StrategyExecutionPanel.java
@@ -218,7 +218,7 @@ public class StrategyExecutionPanel extends JPanel implements IRainbowModelChang
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0 };
 		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
