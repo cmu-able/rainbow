5136e4056d1c44ef5fa65dc8ddaf7ec119ffde72
Jared <indeed@indeed.moe>
2019-08-03 13:51:52 -0400

    initializer: more coverage on Scaffolder.

--- tools/initializer/src/test/java/org/sa/rainbow/initializer/scaffolder/ScaffolderTest.java
+++ tools/initializer/src/test/java/org/sa/rainbow/initializer/scaffolder/ScaffolderTest.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 /**
  * Scaffolder test cases.
@@ -59,15 +60,32 @@ public class ScaffolderTest {
 
     @Test
     public void scaffold() throws Exception {
+        final String DEFAULT_MAPPING_TEMPLATE_PATH = "mapping.yml.ftl";
+
         Map<String, Template> templates = new HashMap<>();
         templates.put("example.txt.ftl", configuration.getTemplate("example.txt.ftl"));
-        templates.put("mapping.yml.ftl", configuration.getTemplate("mapping.yml.ftl"));
+        templates.put(DEFAULT_MAPPING_TEMPLATE_PATH, configuration.getTemplate(DEFAULT_MAPPING_TEMPLATE_PATH));
         List<Variable> variables = Collections.singletonList(new Variable("name"));
         TemplateSet templateSet = new TemplateSet(templates, variables);
         Map<String, Object> configuration = Collections.singletonMap("name", "world");
 
         Scaffolder scaffolder = new Scaffolder(templateSet, configuration);
+
+        assertSame(templateSet, scaffolder.getTemplateSet());
+        assertSame(configuration, scaffolder.getConfiguration());
+        
+        scaffolder.setTemplateSet(templateSet);
+        scaffolder.setConfiguration(configuration);
+
+        assertSame(templateSet, scaffolder.getTemplateSet());
+        assertSame(configuration, scaffolder.getConfiguration());
+
         scaffolder.setBaseDirectory(baseDirectory);
+        assertEquals(baseDirectory, scaffolder.getBaseDirectory());
+
+        scaffolder.setMappingTemplatePath(DEFAULT_MAPPING_TEMPLATE_PATH);
+        assertEquals(DEFAULT_MAPPING_TEMPLATE_PATH, scaffolder.getMappingTemplatePath());
+
         scaffolder.scaffold();
 
         // read the content of expected files, and assert its content.
