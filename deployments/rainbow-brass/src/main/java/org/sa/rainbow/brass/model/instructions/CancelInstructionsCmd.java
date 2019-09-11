cd53617abf12d29127bf0a6d65dcf5eccf907c72
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-09-11 11:14:08 -0400

    Adding cancelInstructions - this is the wrong branch

---
+++ deployments/rainbow-brass/src/main/java/org/sa/rainbow/brass/model/instructions/CancelInstructionsCmd.java
@@ -0,0 +1,59 @@
+package org.sa.rainbow.brass.model.instructions;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.sa.rainbow.core.error.RainbowException;
+import org.sa.rainbow.core.event.IRainbowMessage;
+import org.sa.rainbow.core.models.IModelInstance;
+import org.sa.rainbow.core.models.commands.AbstractRainbowModelOperation;
+import org.sa.rainbow.core.ports.IRainbowMessageFactory;
+
+public class CancelInstructionsCmd extends AbstractRainbowModelOperation<Boolean, InstructionGraphProgress> {
+
+	private LinkedList<IInstruction> m_oldInstructions;
+
+	public CancelInstructionsCmd(IModelInstance<InstructionGraphProgress> model,
+			String target, String... parameters) {
+		super("cancelInstructions", model, target, parameters);
+	}
+
+	@Override
+	public Boolean getResult() throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<? extends IRainbowMessage> getGeneratedEvents(IRainbowMessageFactory messageFactory) {
+		return generateEvents(messageFactory, "cancelInstructions");
+	}
+
+	@Override
+	protected void subExecute() throws RainbowException {
+		Collection<? extends IInstruction> oldInst = getModelContext().getModelInstance().getInstructions();
+		if (oldInst == null)
+			m_oldInstructions = new LinkedList<IInstruction>();
+		else 
+			m_oldInstructions = new LinkedList<IInstruction>(oldInst);
+		getModelContext().getModelInstance().setInstructions(Collections.<IInstruction>emptyList());
+	}
+
+	@Override
+	protected void subRedo() throws RainbowException {
+		subExecute();
+	}
+
+	@Override
+	protected void subUndo() throws RainbowException {
+        getModelContext ().getModelInstance ().setInstructions (m_oldInstructions);
+	}
+
+	@Override
+	protected boolean checkModelValidForCommand(InstructionGraphProgress model) {
+		return true;
+	}
+
+}
