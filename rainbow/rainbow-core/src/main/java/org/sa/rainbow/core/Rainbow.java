71a26cb469f172bef48159ced7a7cb35bd7b833d
Bradley Schmerl <schmerl@cs.cmu.edu>
2019-09-23 09:47:06 -0400

    Added watchdog to wait 10 seconds for Rainbow to shutdown cleanly

--- rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/Rainbow.java
+++ rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/Rainbow.java
@@ -115,6 +115,18 @@ public class Rainbow implements IRainbowEnvironment {
     public void signalTerminate (ExitState exitState) {
         setExitState (exitState);
         signalTerminate ();
+        
+        new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(10000);
+					System.exit(0);
+				} catch (InterruptedException e) {
+				}
+			}      	
+        }).start();
     }
 
     /**
